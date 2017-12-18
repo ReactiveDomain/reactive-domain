512d67e82b88b1ae977516a66640ff611f0ba1b0
Josh Kempner <josh.kempner@perkinelmer.com>
2017-12-18 14:29:44 -0500

    Update to ReactiveUi 7 (#12)
    * Update to ReactiveUi 7
    
    * Add recovery options for failed commands

---
+++ src/ReactiveDomain/ReactiveDomain/ViewObjects/Interactions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive;
+using ReactiveUI;
+
+namespace ReactiveDomain.ViewObjects
+{
+    public static class Interactions
+    {
+        public static readonly Interaction<UserError, RecoveryOptionResult> Errors = new Interaction<UserError, RecoveryOptionResult>();
+
+        /// <summary>
+        /// RecoveryOptionResult describes to the code throwing the error what to do
+        /// once the error is resolved. This is adapted from legacy UserError code.
+        /// </summary>
+        public enum RecoveryOptionResult
+        {
+            /// <summary>
+            /// The operation should be canceled, but it is no longer an error.
+            /// </summary>
+            CancelOperation = 0,
+            /// <summary>
+            /// The operation should be retried with the same parameters.
+            /// </summary>
+            RetryOperation = 1,
+            /// <summary>
+            /// Recovery failed or not possible, you should rethrow as an Exception.
+            /// </summary>
+            FailOperation = 2
+        }
+    }
+}
