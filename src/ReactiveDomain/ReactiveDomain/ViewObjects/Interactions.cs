46df3764379da6a695da086d8c4f461e478bdafa
Josh Kempner <josh.kempner@perkinelmer.com>
2017-12-18 14:15:20 -0500

    Add recovery options for failed commands

--- src/ReactiveDomain/ReactiveDomain/ViewObjects/Interactions.cs
+++ src/ReactiveDomain/ReactiveDomain/ViewObjects/Interactions.cs
@@ -6,6 +6,26 @@ namespace ReactiveDomain.ViewObjects
 {
     public static class Interactions
     {
-        public static readonly Interaction<UserError, Unit> Errors = new Interaction<UserError, Unit>();
+        public static readonly Interaction<UserError, RecoveryOptionResult> Errors = new Interaction<UserError, RecoveryOptionResult>();
+
+        /// <summary>
+        /// RecoveryOptionResult describes to the code throwing the error what to do
+        /// once the error is resolved. This is adapted from legacy UserError code.
+        /// </summary>
+        public enum RecoveryOptionResult
+        {
+            /// <summary>
+            /// The operation should be canceled, but it is no longer an error.
+            /// </summary>
+            CancelOperation = 0,
+            /// <summary>
+            /// The operation should be retried with the same parameters.
+            /// </summary>
+            RetryOperation = 1,
+            /// <summary>
+            /// Recovery failed or not possible, you should rethrow as an Exception.
+            /// </summary>
+            FailOperation = 2
+        }
     }
 }
