bc6d603dfa2fb6b1fd158bee93c78ad56134fbd0
chris.condron <chris@condron.net>
2019-11-05 13:40:50 -0500

    IAggregatedCache and ReadThroughAggregateCache to support CorrelatedStreamStoreRepository with tests

---
+++ src/ReactiveDomain.Foundation/StreamStore/IAggregateCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReactiveDomain.Foundation.StreamStore {
+    /// <summary>
+    /// While it might seem more natural to save and restore the event set behind the aggregate,
+    /// this cache stores only the collapsed state in the aggregate  
+    /// </summary>
+    public interface IAggregateCache: IDisposable
+    {
+        bool GetById<TAggregate>(Guid id, out TAggregate aggregate) where TAggregate : class, IEventSource;
+        bool Save(IEventSource aggregate);
+        bool Remove(Guid id);
+        void Clear();
+    }
+}
---
+++ src/ReactiveDomain.Foundation/StreamStore/ReadThroughAggregateCache.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReactiveDomain.Foundation.StreamStore {
+    /// <summary>
+    /// This implementation assumes that the cached aggregate may have changes since it was last seen
+    ///
+    /// Therefore the aggregate is cached on first retrieve and the store is checked for updates upon
+    /// each subsequent retrieval
+    ///
+    /// Also retrieving an aggregate at anything other than latest is disabled
+    ///
+    /// Cache management (e.g. eviction) is the responsibility of the caller/external owner
+    ///
+    /// Save failures will clear the aggregate from the cache and return false
+    /// </summary>
+    public class ReadThroughAggregateCache :IAggregateCache, IDisposable {
+
+        private readonly IRepository _baseRepository;
+        private readonly Dictionary<Guid, IEventSource> _knownAggregates = new Dictionary<Guid, IEventSource>();
+        public ReadThroughAggregateCache(IRepository baseRepository) {
+            _baseRepository = baseRepository;
+        }
+
+
+        public bool GetById<TAggregate>(Guid id, out TAggregate aggregate) where TAggregate : class, IEventSource {
+            try {
+                aggregate = GetById<TAggregate>(id);
+                return true;
+            }
+            catch (Exception) {
+                aggregate = null;
+                return false;
+            }
+        }
+        private TAggregate GetById<TAggregate>(Guid id) where TAggregate : class, IEventSource {
+            if (_knownAggregates.TryGetValue(id, out var aggregate)) {
+                try {
+                    _baseRepository.UpdateToCurrent(aggregate);
+                }
+                catch(InvalidOperationException ex) {
+                    _knownAggregates.Remove(aggregate.Id);
+                    throw new Exception("Persisted version changed with recorded events in aggregate.", ex);
+                }
+                catch (AggregateVersionException ex) {
+                    _knownAggregates.Remove(aggregate.Id);
+                    throw new Exception("Persisted version mismatch.", ex);
+                }
+                return aggregate as TAggregate;
+            }
+
+            aggregate = _baseRepository.GetById<TAggregate>(id);
+            _knownAggregates.Add(id, aggregate);
+
+            return (TAggregate)aggregate;
+        }
+        public bool Save(IEventSource aggregate) {
+            try {
+                _baseRepository.Save(aggregate);
+                if (!_knownAggregates.ContainsKey(aggregate.Id)) {
+                    _knownAggregates.Add(aggregate.Id, aggregate);
+                }
+                else {
+                    _knownAggregates[aggregate.Id] = aggregate;
+                }
+
+                return true;
+            }
+            catch {
+                _knownAggregates.Remove(aggregate.Id);
+                return false;
+            }
+
+            
+        }
+        public bool Remove(Guid id) {
+            return _knownAggregates.Remove(id);
+        }
+        public void Clear() {
+            _knownAggregates.Clear();
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                _knownAggregates.Clear();
+            }
+        }
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
