42584e589180bf79c1687592bc977ed3f3d26121
Josh Kempner <josh.kempner@perkinelmer.com>
2020-08-05 10:40:48 -0400

    Changes to CorrelatedStreamStoreRepository
    - Add a ctor overload to inject both an IRepository and a cache factory.
    - Fix a bug in the Dispose(bool) method that could cause an exception to be thrown.

--- src/ReactiveDomain.Foundation/StreamStore/CorrelatedStreamStoreRepository.cs
+++ src/ReactiveDomain.Foundation/StreamStore/CorrelatedStreamStoreRepository.cs
@@ -3,33 +3,39 @@ using ReactiveDomain.Foundation.StreamStore;
 using ReactiveDomain.Messaging;
 
 // ReSharper disable once CheckNamespace
-namespace ReactiveDomain.Foundation{
-    public class CorrelatedStreamStoreRepository:ICorrelatedRepository, IDisposable
+namespace ReactiveDomain.Foundation {
+    public class CorrelatedStreamStoreRepository : ICorrelatedRepository, IDisposable
     {
         private readonly IRepository _repository;
         private readonly IAggregateCache _cache = null;
-        public CorrelatedStreamStoreRepository(IRepository repository) {
-            _repository = repository;
-        }
         public CorrelatedStreamStoreRepository(
-            IStreamNameBuilder streamNameBuilder,
-            IStreamStoreConnection streamStoreConnection,
-            IEventSerializer eventSerializer,
-            Func<IRepository,IAggregateCache> cacheFactory = null) {
-            _repository = new StreamStoreRepository(streamNameBuilder,streamStoreConnection,eventSerializer);
+            IRepository repository,
+            Func<IRepository, IAggregateCache> cacheFactory = null) {
+            _repository = repository;
             if (cacheFactory != null) {
                 _cache = cacheFactory(_repository);
             }
         }
-        
+
+        public CorrelatedStreamStoreRepository(
+            IStreamNameBuilder streamNameBuilder,
+            IStreamStoreConnection streamStoreConnection,
+            IEventSerializer eventSerializer,
+            Func<IRepository, IAggregateCache> cacheFactory = null) {
+            _repository = new StreamStoreRepository(streamNameBuilder, streamStoreConnection, eventSerializer);
+            if (cacheFactory != null) {
+                _cache = cacheFactory(_repository);
+            }
+        }
+
         public bool TryGetById<TAggregate>(Guid id, out TAggregate aggregate, ICorrelatedMessage source) where TAggregate : AggregateRoot, IEventSource {
             return TryGetById(id, int.MaxValue, out aggregate, source);
         }
 
         public TAggregate GetById<TAggregate>(Guid id, ICorrelatedMessage source) where TAggregate : AggregateRoot, IEventSource {
-           return GetById<TAggregate>(id, int.MaxValue, source);
+            return GetById<TAggregate>(id, int.MaxValue, source);
         }
-        
+
         public bool TryGetById<TAggregate>(Guid id, int version, out TAggregate aggregate, ICorrelatedMessage source) where TAggregate : AggregateRoot, IEventSource {
             try {
                 aggregate = GetById<TAggregate>(id, version, source);
@@ -40,33 +46,34 @@ namespace ReactiveDomain.Foundation{
                 return false;
             }
         }
-       
-        public TAggregate GetById<TAggregate>(Guid id, int version, ICorrelatedMessage source) where TAggregate : AggregateRoot, IEventSource
-        {
+
+        public TAggregate GetById<TAggregate>(Guid id, int version, ICorrelatedMessage source) where TAggregate : AggregateRoot, IEventSource {
             TAggregate agg = null;
             _cache?.GetById(id, out agg);
 
-            if(agg == null || agg.Version > version) {
+            if (agg == null || agg.Version > version) {
                 agg = _repository.GetById<TAggregate>(id, version);
             }
-            
+
             ((ICorrelatedEventSource)agg).Source = source;
             return agg;
         }
 
         public void Save(IEventSource aggregate) {
             if (_cache != null) {
-                _cache.Save( aggregate);
+                _cache.Save(aggregate);
             }
             else {
                 _repository.Save(aggregate);
             }
         }
+
         protected virtual void Dispose(bool disposing) {
             if (disposing) {
-                _cache.Dispose();
+                _cache?.Dispose();
             }
         }
+
         public void Dispose() {
             Dispose(true);
             GC.SuppressFinalize(this);
