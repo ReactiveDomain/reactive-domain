eb3b41bcfbaac736586793cd106cbf34a2e0b34a
chris.condron <chris@condron.net>
2021-01-04 07:39:38 -0500

    add client store and resource store to storage project with static data

---
+++ src/ReactiveDomain.IdentityServer4.Storage/ClientStore.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+
+namespace ReactiveDomain.IdentityServer4.Storage {
+    public class ClientStore : IClientStore
+    {
+        private readonly Client _client = new Client
+        {
+            ClientId = "client",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedScopes = { "api2", "api1" }
+        };
+        public async Task<Client> FindClientByIdAsync(string clientId)
+        {
+            return await Task.FromResult(_client);
+        }
+    }
+}
---
+++ src/ReactiveDomain.IdentityServer4.Storage/ResourcesStore.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+
+namespace ReactiveDomain.IdentityServer4.Storage {
+    public class ResourcesStore : IResourceStore
+    {
+        private readonly Resources _resources = new Resources();
+
+        public ResourcesStore()
+        {
+            _resources.IdentityResources.Add(new IdentityResources.OpenId());
+            _resources.ApiScopes.Add(new ApiScope("api1", "My API"));
+            _resources.ApiScopes.Add(new ApiScope("api2", "My Other API"));
+        }
+
+
+        public async Task<IEnumerable<IdentityResource>> FindIdentityResourcesByScopeNameAsync(
+            IEnumerable<string> scopeNames)
+        {
+            var names = scopeNames.ToHashSet();
+            var resources = new List<IdentityResource>();
+            if (!names.IsNullOrEmpty())
+            {
+                resources.AddRange(_resources.IdentityResources.Where(r => names.Contains(r.Name, StringComparer.Ordinal)));
+            }
+            return await Task.FromResult(resources);
+        }
+
+        public async Task<IEnumerable<ApiScope>> FindApiScopesByNameAsync(IEnumerable<string> scopeNames)
+        {
+            var names = scopeNames.ToHashSet();
+            var apiScopes = new List<ApiScope>();
+            if (!names.IsNullOrEmpty())
+            {
+                apiScopes.AddRange(_resources.ApiScopes.Where(r => names.Contains(r.Name, StringComparer.Ordinal)));
+            }
+            return await Task.FromResult(apiScopes);
+        }
+
+        public async Task<IEnumerable<ApiResource>> FindApiResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            var names = scopeNames.ToHashSet();
+            var apiResources = new List<ApiResource>();
+            if (!names.IsNullOrEmpty())
+            {
+                foreach (var apiResource in _resources.ApiResources) {
+                    var scopesHash = apiResource.Scopes.ToHashSet();
+                    if (scopesHash.Overlaps(names)) {
+                        apiResources.Add(apiResource);
+                    }
+                }
+            }
+            return await Task.FromResult(apiResources);
+        }
+
+        public async Task<IEnumerable<ApiResource>> FindApiResourcesByNameAsync(IEnumerable<string> apiResourceNames)
+        {
+            var names = apiResourceNames.ToHashSet();
+            var apiResources = new List<ApiResource>();
+            if (!names.IsNullOrEmpty())
+            {
+                apiResources.AddRange(_resources.ApiResources.Where(r => names.Contains(r.Name, StringComparer.Ordinal)));
+            }
+            return await Task.FromResult(apiResources);
+        }
+
+        public async Task<Resources> GetAllResourcesAsync()
+        {
+            return await Task.FromResult(_resources);
+        }
+    }
+}
