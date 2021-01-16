ccafc1a35a394902bf8002cae3a35cfc09d38dea
chris.condron <chris@condron.net>
2021-01-16 12:17:43 -0500

    add security policy interface

---
+++ src/ReactiveDomain.Users/Policy/ISecurityPolicy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using EventStore.ClientAPI;
+using ReactiveDomain.Users.ReadModels;
+
+namespace ReactiveDomain.Users.Policy
+{
+    public interface ISecurityPolicy {
+        public  string ApplicationName { get; }
+        public  string ApplicationVersion { get;  }
+        public  IReadOnlyList<Role> GetUserRoles(Guid userId);
+        public  IReadOnlyList<Permission> GetUserPermissions(Guid userId);
+        public  void ConfigurePolicy(IEventStoreConnection conn);
+    }
+}
