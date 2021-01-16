0d540bab00f7b86fc918e0482b047a3b6707590a
chris.condron <chris@condron.net>
2021-01-16 13:39:50 -0500

    update Isecurity policy with todos

--- src/ReactiveDomain.Users/Policy/ISecurityPolicy.cs
+++ src/ReactiveDomain.Users/Policy/ISecurityPolicy.cs
@@ -5,11 +5,25 @@ using ReactiveDomain.Users.ReadModels;
 
 namespace ReactiveDomain.Users.Policy
 {
+    //application security policy read model
+    //defined in the reactive domain users read models
     public interface ISecurityPolicy {
         string ApplicationName { get; }
         string ApplicationVersion { get;  }
         IReadOnlyList<Role> GetUserRoles(Guid userId);
         IReadOnlyList<Permission> GetUserPermissions(Guid userId);
-        void ConfigurePolicy(IEventStoreConnection conn);
+    }
+    //defined in application
+    public interface IPolicyDefinition {
+        //todo: list of roles
+        //todo: list of permissions
+        //todo: role hierarchy definition
+        //todo: role => permission mapping
+        //todo: application name
+        //todo: application version
+    }
+    //defined in reactive domain users domain services
+    public interface IConfigureSecurity {
+        void Configure(IPolicyDefinition definition, IEventStoreConnection conn);
     }
 }
