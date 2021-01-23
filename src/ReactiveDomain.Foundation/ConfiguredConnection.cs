9bb94a5e7e303956c863e0f370ad86b5436c6b61
chris.condron <chris@condron.net>
2021-01-23 17:32:27 -0500

    update configured connection to include repos

--- src/ReactiveDomain.Foundation/ConfiguredConnection.cs
+++ src/ReactiveDomain.Foundation/ConfiguredConnection.cs
@@ -1,29 +1,51 @@
-﻿namespace ReactiveDomain.Foundation {
-    public class ConfiguredConnection : IConfiguredConnection {
-        
+﻿using ReactiveDomain.Foundation.StreamStore;
+
+namespace ReactiveDomain.Foundation
+{
+    public class ConfiguredConnection : IConfiguredConnection
+    {
+
         public ConfiguredConnection(
             IStreamStoreConnection conn,
             IStreamNameBuilder namer,
-            IEventSerializer serializer) {
+            IEventSerializer serializer)
+        {
             Connection = conn;
             StreamNamer = namer;
             Serializer = serializer;
         }
 
-        public IStreamStoreConnection Connection { get;  }
+        public IStreamStoreConnection Connection { get; }
         public IStreamNameBuilder StreamNamer { get; }
 
         public IEventSerializer Serializer { get; }
 
-        public IListener GetListener(string name) {
-            return new StreamListener(name,Connection,StreamNamer,Serializer);
+        public IListener GetListener(string name)
+        {
+            return new StreamListener(name, Connection, StreamNamer, Serializer);
         }
-        public IListener GetQueuedListener(string name) {
-            return new QueuedStreamListener(name,Connection,StreamNamer,Serializer);
+        public IListener GetQueuedListener(string name)
+        {
+            return new QueuedStreamListener(name, Connection, StreamNamer, Serializer);
         }
 
-        public IStreamReader GetReader(string name) {
-            return new StreamReader(name,Connection,StreamNamer,Serializer);
+        public IStreamReader GetReader(string name)
+        {
+            return new StreamReader(name, Connection, StreamNamer, Serializer);
+        }
+
+        public IRepository GetRepository(bool caching = false)
+        {
+            IRepository repo = new StreamStoreRepository(StreamNamer, Connection, Serializer);
+            return caching
+                ? new ReadThroughAggregateCache(repo)
+                : repo;
+        }
+
+        public ICorrelatedRepository GetCorrelatedRepository(
+            IRepository baseRepository = null,bool caching = false)
+        {
+            return new CorrelatedStreamStoreRepository(baseRepository ?? GetRepository(caching));
         }
 
     }
--- src/ReactiveDomain.Foundation/IConfiguredConnection.cs
+++ src/ReactiveDomain.Foundation/IConfiguredConnection.cs
@@ -8,5 +8,8 @@
         IListener GetListener(string name);
         IListener GetQueuedListener(string name);
         IStreamReader GetReader(string name);
+        IRepository GetRepository(bool caching = false);
+        ICorrelatedRepository GetCorrelatedRepository(IRepository baseRepository = null, bool caching = false);
+
     }
 }
