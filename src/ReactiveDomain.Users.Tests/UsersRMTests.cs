108badfd0b5a4dc429172aec38810df6cbfd317f
Chris Condron <chris@condron.net>
2021-10-20 19:15:54 -0400

    update userRm tests

--- src/ReactiveDomain.Users.Tests/UsersRMTests.cs
+++ src/ReactiveDomain.Users.Tests/UsersRMTests.cs
@@ -2,6 +2,7 @@
 using ReactiveDomain.Foundation;
 using ReactiveDomain.Messaging;
 using ReactiveDomain.Testing;
+using ReactiveDomain.Users.Domain;
 using ReactiveDomain.Users.Messages;
 using ReactiveDomain.Users.ReadModels;
 using ReactiveDomain.Users.Services;
@@ -32,95 +33,104 @@ namespace ReactiveDomain.Users.Tests
         public UsersRMTests()
         {
             _fixture = new MockRepositorySpecification();
-            
+            AddUsers();
             _rm = new UsersRm(_fixture.ConfiguredConnection);
-           // AddUsers();
+
         }
 
-        public void Dispose()
-        {
-            _rm?.Dispose();
-        }
-/*
+
+
         [Fact]
         public void correct_users_exist()
         {
-            Assert.True(_rm.UserExists(_id1));
-            Assert.True(_rm.UserExists(AuthProvider, AuthDomain, SubjectId, out _));
-            Assert.True(_rm.UserExists(_id2));
-            Assert.True(_rm.UserExists(AuthProvider, AuthDomain, SubjectId2, out _));
+            Assert.True(_rm.UsersById.ContainsKey(_id1));
+            Assert.True(_rm.HasUser(SubjectId, AuthDomain, out _));
+            Assert.True(_rm.UsersById.ContainsKey(_id2));
+            Assert.True(_rm.HasUser(SubjectId2, AuthDomain, out _));
 
-            Assert.False(_rm.UserExists(Guid.NewGuid()));
-            Assert.False(_rm.UserExists(Guid.Empty));
-            Assert.False(_rm.UserExists(AuthProvider, AuthDomain, "bogus", out _));
-            Assert.False(_rm.UserExists(AuthProvider, "bogus", SubjectId, out _));
-            Assert.False(_rm.UserExists("bogus", AuthDomain, SubjectId, out _));
+            Assert.False(_rm.UsersById.ContainsKey(Guid.NewGuid()));
+            Assert.False(_rm.UsersById.ContainsKey(Guid.Empty));
+            Assert.False(_rm.HasUser(AuthDomain, "bogus", out _));
+            Assert.False(_rm.HasUser("bogus", SubjectId, out _));
+
         }
 
         [Fact]
-        public void can_get_users_by_name()
+        public void can_get_user_id_by_SID()
         {
-            Assert.True(_rm.TryGetUserId(AuthProvider, AuthDomain, SubjectId,  out var id1));
+            Assert.True(_rm.HasUser(SubjectId, AuthDomain, out var id1));
             Assert.Equal(_id1, id1);
-            Assert.True(_rm.TryGetUserId(AuthProvider, AuthDomain, SubjectId2,  out var id2));
+            Assert.True(_rm.HasUser(SubjectId2, AuthDomain, out var id2));
             Assert.Equal(_id2, id2);
         }
-        [Fact]
-        public void can_get_users_by_userSidFromAuthProvider()
-        {
-            Assert.True(_rm.TryGetUserId(AuthProvider, AuthDomain, SubjectId, out var id1));
-            Assert.Equal(_id1, id1);
-            Assert.True(_rm.TryGetUserId(AuthProvider, AuthDomain, SubjectId2, out var id2));
-            Assert.Equal(_id2, id2);
-        }
+
+
         [Fact]
         public void cannot_get_nonexistent_user()
         {
-            Assert.False(_rm.TryGetUserId("bogus", AuthDomain, UserName,  out _));
-            Assert.False(_rm.TryGetUserId(AuthProvider, "bogus", UserName,  out _));
-            Assert.False(_rm.TryGetUserId(AuthProvider, AuthDomain, "bogus",  out _));
+
+            Assert.False(_rm.HasUser(SubjectId, "bogus", out _));
+            Assert.False(_rm.HasUser("bogus", AuthDomain, out _));
         }
 
         private void AddUsers()
         {
             var evt1 = MessageBuilder.New(
                 () => new UserMsgs.UserCreated(
-                            _id1,
+                            _id1));
+            var evt2 = MessageBuilder.New(
+               () => new UserMsgs.UserDetailsUpdated(
+                           _id1,
+                            FullName,
+                            GivenName,
+                            Surname,
+                            Email));
+            var evt3 = MessageBuilder.New(
+               () => new UserMsgs.AuthDomainMapped(
+                           _id1,
                             SubjectId,
                             AuthProvider,
                             AuthDomain,
-                            UserName,
+                            UserName));
+
+            var evt4 = MessageBuilder.New(
+                           () => new UserMsgs.UserCreated(
+                                       _id2));
+            var evt5 = MessageBuilder.New(
+               () => new UserMsgs.UserDetailsUpdated(
+                           _id2,
                             FullName,
                             GivenName,
                             Surname,
                             Email));
-            var evt2 = MessageBuilder.New(
-                () => new UserMsgs.UserCreated(
-                            _id2,
+            var evt6 = MessageBuilder.New(
+               () => new UserMsgs.AuthDomainMapped(
+                           _id2,
                             SubjectId2,
                             AuthProvider,
                             AuthDomain,
-                            UserName2,
-                            FullName,
-                            GivenName,
-                            Surname,
-                            Email));
+                            UserName2));
+
+
             var stream1 = _fixture.StreamNameBuilder.GenerateForAggregate(typeof(User), _id1);
             var stream2 = _fixture.StreamNameBuilder.GenerateForAggregate(typeof(User), _id2);
             _fixture.StreamStoreConnection.AppendToStream(
                 stream1,
                 ExpectedVersion.Any,
                 null,
-                _fixture.EventSerializer.Serialize(evt1));
-            _fixture.RepositoryEvents.WaitForMsgId(evt1.MsgId, TimeSpan.FromMilliseconds(100));
+                new[] { _fixture.EventSerializer.Serialize(evt1), _fixture.EventSerializer.Serialize(evt2), _fixture.EventSerializer.Serialize(evt3) });
+            _fixture.RepositoryEvents.WaitForMsgId(evt3.MsgId, TimeSpan.FromMilliseconds(100));
             _fixture.StreamStoreConnection.AppendToStream(
                 stream2,
                 ExpectedVersion.Any,
                 null,
-                _fixture.EventSerializer.Serialize(evt2));
-            _fixture.RepositoryEvents.WaitForMsgId(evt2.MsgId, TimeSpan.FromMilliseconds(100));
+                new[] { _fixture.EventSerializer.Serialize(evt4), _fixture.EventSerializer.Serialize(evt5), _fixture.EventSerializer.Serialize(evt6) });
+            _fixture.RepositoryEvents.WaitForMsgId(evt6.MsgId, TimeSpan.FromMilliseconds(100));
             _fixture.ClearQueues();
         }
-*/
+        public void Dispose()
+        {
+            _rm?.Dispose();
+        }
     }
 }
