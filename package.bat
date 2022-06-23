29bc4b2674b204b8b9f30f751a23e6044dfaab36
Chris Condron <chris@condron.net>
2022-06-23 12:09:49 -0400

    Policy tool published to consuming project output in debug packages

--- package.bat
+++ package.bat
@@ -4,5 +4,5 @@ del .\bld\pub\*.* /q
 dotnet restore .\src\ReactiveDomain.sln
 dotnet build .\src\ReactiveDomain.sln -c Debug
 dotnet publish .\src\ReactiveDomain.PolicyTool\ReactiveDomain.PolicyTool.csproj -p:PublishProfile=FolderProfile
-powershell -Command "& {.\tools\CreateDebugNuget.ps1 -rc003}"
+powershell -Command "& {.\tools\CreateDebugNuget.ps1 -rc009}"
 
