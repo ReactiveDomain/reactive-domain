73fe5bbda154a52d94a37fd5c42626c3f5278afc
Josh Kempner <josh.kempner@perkinelmer.com>
2022-07-08 14:04:44 -0400

    Add methods to get commands/permissions from a msgs class

---
+++ src/ReactiveDomain.Policy/Permissions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ReactiveDomain.Messaging;
+
+namespace ReactiveDomain.Policy
+{
+    public static class Permissions
+    {
+        private static readonly Type CommandType = typeof(Command);
+
+        public static Permission[] GetCommandPermissions(Type type)
+        {
+            return GetCommands(type).Select(t => new Permission(t)).ToArray();
+        }
+
+        public static IEnumerable<Type> GetCommands(Type type)
+        {
+            return type.GetNestedTypes().Where(t => CommandType.IsAssignableFrom(t));
+        }
+    }
+}
