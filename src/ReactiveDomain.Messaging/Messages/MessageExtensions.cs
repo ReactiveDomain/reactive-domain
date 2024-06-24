28eeacf7b8cc2c4857621f39aa9ab10fa7fe6e88
Josh Kempner <joshkempner@users.noreply.github.com>
2024-06-24 18:01:27 -0400

    Adds TryReadMetadataum extension method for Message

--- src/ReactiveDomain.Messaging/Messages/MessageExtensions.cs
+++ src/ReactiveDomain.Messaging/Messages/MessageExtensions.cs
@@ -21,13 +21,28 @@
         /// Reads the metadata of the specified type from a <see cref="Message"/>.
         /// </summary>
         /// <typeparam name="T">The type of the metadata.</typeparam>
-        /// <param name="msg">The message to update.</param>
-        /// <returns>The metadata from the message or a default object of the specified type if no metadatum of that type is found on the message.</returns>
+        /// <param name="msg">The message to read.</param>
+        /// <returns>The metadata from the message.</returns>
+        /// <exception cref="MetadatumNotFoundException">Thrown if no metadatum of that type is found on the message.</exception>
         public static T ReadMetadatum<T>(this Message msg)
         {
             var mds = (IMetadataSource)msg;
             var md = mds.ReadMetadata() ?? mds.Initialize();
             return md.Read<T>();
+        }
+
+        /// <summary>
+        /// Tries to read the metadata of the specified type from a <see cref="Message"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the metadata.</typeparam>
+        /// <param name="msg">The message to read.</param>
+        /// <param name="metadatum">The metadata from the message.</param>
+        /// <returns>The metadata from the message or a default object of the specified type if no metadatum of that type is found on the message.</returns>
+        public static bool TryReadMetadatum<T>(this Message msg, out T metadatum)
+        {
+            var mds = (IMetadataSource)msg;
+            var md = mds.ReadMetadata() ?? mds.Initialize();
+            return md.TryRead(out metadatum);
         }
     }
 }
