31c46d377a4b11890e08808b13a40f07c26dde1b
joshkempner <josh@kempner.net>
2026-03-21 11:11:38 -0400

    Adds net10.0 to supported frameworks
    - Adds net10.0 to build.props & nuspec files
    - Adds 10.0 to the CI build file
    - Builds PolicyTool using .NET 10 for use in the ReactiveDomain.Policy nuget package.

--- package.bat
+++ package.bat
@@ -3,6 +3,6 @@ del .\bld\Debug\*.* /q
 del .\bld\pub\*.* /q
 dotnet restore .\src\ReactiveDomain.sln
 dotnet build .\src\ReactiveDomain.sln -c Debug
-dotnet publish .\src\ReactiveDomain.PolicyTool\ReactiveDomain.PolicyTool.csproj -p:PublishProfile=FolderProfile
+dotnet publish .\src\ReactiveDomain.PolicyTool\ReactiveDomain.PolicyTool.csproj -p:PublishProfile=FolderProfile --framework net10.0
 pwsh.exe -Command "& {.\tools\CreateDebugNuget.ps1 -local002}"
 
