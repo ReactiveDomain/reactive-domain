202eefe924601e251269254f53ec8bd19a77ebb5
Josh Kempner <joshkempner@users.noreply.github.com>
2026-03-21 11:32:26 -0400

    Adds net10.0 to supported frameworks (#190)

--- publish.bat
+++ publish.bat
@@ -3,6 +3,6 @@ del .\bld\Release\*.* /q
 del .\bld\pub\*.* /q
 dotnet restore .\src\ReactiveDomain.sln
 dotnet build .\src\ReactiveDomain.sln -c Release
-dotnet publish .\src\ReactiveDomain.PolicyTool\ReactiveDomain.PolicyTool.csproj -c Release -p:PublishProfile=FolderProfile
+dotnet publish .\src\ReactiveDomain.PolicyTool\ReactiveDomain.PolicyTool.csproj -c Release -p:PublishProfile=FolderProfile --framework net10.0
 pwsh.exe -Command "& {.\tools\CreateNuget.ps1}"
 
