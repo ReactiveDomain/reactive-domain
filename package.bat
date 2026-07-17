123c751a7f21be25836c32b7339d042f4706e565
joshkempner <josh@kempner.net>
2026-07-17 16:43:55 -0400

    Improvements to packaging
    - Adds explicit `IsPackable` tag to all csproj files.
    - Adds `PrivateAssets` attribute to appropriate `PackageReference` tags.
    - Adds the repo README to all nuspec files.
    - Adds tags and repository info to all nuspec files.
    - Refines the descriptions in nuspec files
    - Removes unnecessary references from nuspec files
    - Builds PolicyTool against both .NET 8 & 10, and includes both in the package.

--- package.bat
+++ package.bat
@@ -1,8 +1,10 @@
 del .\nupkgs\*.* /q
 del .\bld\Debug\*.* /q
 del .\bld\pub\*.* /q
+del .\bld\tools\*.* /q
 dotnet restore .\src\ReactiveDomain.sln
 dotnet build .\src\ReactiveDomain.sln -c Debug
+dotnet publish .\src\ReactiveDomain.PolicyTool\ReactiveDomain.PolicyTool.csproj -p:PublishProfile=FolderProfile --framework net8.0
 dotnet publish .\src\ReactiveDomain.PolicyTool\ReactiveDomain.PolicyTool.csproj -p:PublishProfile=FolderProfile --framework net10.0
 pwsh.exe -Command "& {.\tools\CreateDebugNuget.ps1 -local002}"
 
